d62abf291913d10f81b195c215d3e3014c455da6
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-18 20:34:56 +0200

    bloglist backend 4.22

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,16 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
+// const User = require('../models/user')
 
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.startsWith('Bearer ')) {
+//     return authorization.replace('Bearer ', '')
+//   }
+//   return null
+// }
 
 blogsRouter.get('/', async (_request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -22,13 +22,14 @@ blogsRouter.post('/', async (request, response) => {
 
   const body = request.body
 
+  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  // if (!decodedToken.id) {
+  //   return response.status(401).json({ error: 'token invalid' })
+  // }
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
+  // const user = await User.findById(decodedToken.id)
 
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({ ...request.body, author: user.name, user: user._id, likes: body.likes || 0 })
 
@@ -44,22 +45,29 @@ blogsRouter.post('/', async (request, response) => {
 
   response.status(201).json(savedBlog)
 
-  // try {
 
-  // } catch (err) {
-  //   response.status(400).json({ error: 'missing blog data' })
-  // }
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
 
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-  // try {
-
-  // } catch (error) {
-  //   response.status(410).end()
+  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  // if (!decodedToken.id) {
+  //   return response.status(401).json({ error: 'token invalid' })
   // }
+
+  // const user = await User.findById(decodedToken.id)
+
+  const user = request.user
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() === user.id) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    response.status(401).json({ error: 'Deleting post not possible - user unauthorized' })
+  }
+
 })
 
 blogsRouter.put('/:id', async (request, response) => {
@@ -75,12 +83,7 @@ blogsRouter.put('/:id', async (request, response) => {
 
   const updatedPost = await Blog.findByIdAndUpdate(request.params.id, post, { new: true })
   response.json(updatedPost)
-  // try {
 
-  // }
-  // catch (error) {
-  //   response.status(400).end()
-  // }
 })
 
 
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, _response, next) => {
   logger.info('Method:', request.method)
@@ -8,11 +10,36 @@ const requestLogger = (request, _response, next) => {
   next()
 }
 
+const tokenExtractor = (request, _response, next) => {
+  const auth = request.get('authorization')
+
+  if (auth && auth.startsWith('Bearer ')) {
+    request.token = auth.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+
+  next()
+}
+
+const userExtractor = async (request, _response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    request.user = null
+  } else {
+    const user = await User.findById(decodedToken.id)
+    request.user = user
+  }
+
+  next()
+}
+
 const unknownEndpoint = (_request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-const errorHandler = (error, request, response, next) => {
+const errorHandler = (error, _request, response, next) => {
   logger.error(error.message)
 
   if (error.name === 'CastError') {
@@ -20,7 +47,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name ===  'JsonWebTokenError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(401).json({ error: 'unauthorized' })
   } else if (error.name === 'TokenExpiredError') {
     return response.status(401).json({ error: 'token expired' })
   }
@@ -31,5 +58,7 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor,
+  userExtractor
 }
