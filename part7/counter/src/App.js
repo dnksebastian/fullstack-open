3033efac2b48717c4b4206e72b633c91e970f672
dnksebastian <sebastian.dulniawka@gmail.com>
2023-06-23 18:59:33 +0200

    part7 counter-hooks

---
+++ part7/counter/src/App.js
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+import useCounter from './useCounter'
+
+const App = () => {
+  const counter = useCounter()
+
+  return (
+    <div>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>
+        plus
+      </button>
+      <button onClick={counter.decrease}>
+        minus
+      </button>      
+      <button onClick={counter.zero}>
+        zero
+      </button>
+    </div>
+  )
+}
+
+export default App;
